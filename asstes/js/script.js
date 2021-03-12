8baa2a140c9d23c0cc8e879e2ee717004eaa3e0e
hakeem235 <hakeemkoo@gmail.com>
2021-03-12 12:40:17 -0500

    add var in the javascript flile

---
+++ asstes/js/script.js
@@ -0,0 +1,19 @@
+// Start Screen variables
+var startBtn = document.getElementById("start-btn");
+var startQuiz = document.getElementById("start__quiz");
+
+// Quiz screen variables
+var questionScetion = document.getElementById("question-section");
+var questionTitle = document.getElementById("q-title");
+var choicesEl = document.getElementById("choices");
+var rightWrongEl = document.getElementById("right-wrong");
+var buttonsEL = document.getElementById("buttons");
+
+// End Screen Variables
+var endScreen = document.getElementById("end__quiz");
+var userName = document.getElementById("user-name");
+var nameSubmit = document.getElementById("name-submit");
+
+// Final Score Screen
+var finalScoreEl = document.getElementById("final-score");
+var highScoreScreen = document.getElementById("high_score_page");
